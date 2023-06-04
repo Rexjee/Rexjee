e54265cc9eb1d073962f9d69c1ecffa5fa2b81c7
Rexjee <lia2azzam@gmail.com>
2023-06-04 22:46:11 +0800

    Project Praktikum

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+    $project_location = "/masjid";
+    $me = $project_location;
+ 
+    $request = $_SERVER['REQUEST_URI'];
+
+      switch ($request) {
+        case $me.'/' :
+            require "source/pages/home.php";
+            break;
+        case $me.'/petugas' :
+            require "source/pages/petugas.php";
+            break;
+        case $me.'/mtk' :
+            require "source/pages/mtk.html";
+            break;
+        case $me.'/fardhu' :
+            require "source/pages/fardhu.php";
+            break;
+        case $me.'/jumat' :
+              require "source/pages/jumat.php";
+              break;
+        case $me.'/pengajian' :
+                require "source/pages/pengajian.php";
+                break;
+        case $me.'/reportPengajian' :
+                    require "source/pages/reportPengajian.php";
+                    break;
+        case $me.'/uji' :
+                require "source/pages/pageUji.php";
+                break;
+        case $me.'/laporanuji' :
+                require "source/pages/laporanUji.php";
+                break;
+        default:
+            http_response_code(404);
+            require "source/pages/404.php";
+            break;
+        
+    }
+
+?>
